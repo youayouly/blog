2184e8f2c401d557ddda2653f4bb7ad78b71966e
youayouly <youayouly@gmail.com>
2026-04-23 19:12:48 +0800

    feat: add responsive layout for project cards (4/3/2/1 columns)

--- docs/.vuepress/components/ProjectCardsGrid.vue
+++ docs/.vuepress/components/ProjectCardsGrid.vue
@@ -314,19 +314,25 @@ const visibleItems = computed(() => {
 
 .lk-proj-cards__grid {
   display: grid;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-template-columns: repeat(4, minmax(0, 1fr));
   gap: 20px;
   width: 100%;
   margin: 0;
 }
 
-@media (max-width: 1024px) {
+@media (max-width: 1799px) {
+  .lk-proj-cards__grid {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+}
+
+@media (max-width: 1399px) {
   .lk-proj-cards__grid {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
 }
 
-@media (max-width: 719px) {
+@media (max-width: 1023px) {
   .lk-proj-cards__grid {
     grid-template-columns: 1fr;
   }
